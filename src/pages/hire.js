be7609294729e93b253ff9ba3a78ed0a7fd34681
Egor Sadanov <egorsadanov@yandex.ru>
2020-09-15 14:34:36 +1000

    hire page update

---
+++ src/pages/hire.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react'
+import Alert from 'react-bootstrap/Alert'
+import Container from 'react-bootstrap/Container'
+
+
+const Hire = () => (
+	<p>yo</p>
+)
+
+function Hiring () {
+	return (
+		<Container>
+			<p>yo ty tut?</p>
+		</Container>
+	)
+}
+
+export default Hire
