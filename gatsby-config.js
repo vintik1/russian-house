ed2282f9499d2425d7accac066e1e397c066395e
Egor Sadanov <egorsadanov@yandex.ru>
2020-09-25 13:28:14 +1000

    config update

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,13 +19,13 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    // {
-    //   resolve: `gatsby-source-filesystem`,
-    //   options: {
-    //     name: `pages`,
-    //     path: `${__dirname}/src/pages`,
-    //   },
-    // },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages`,
+      },
+    },
     // {
     //   resolve: `gatsby-plugin-s3`,
     //   options: {
