ca43152cc89945bc11a21ca252f08aaf50d81f39
Egor Sadanov <egorsadanov@yandex.ru>
2020-10-06 00:05:44 +1100

    footer form update

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,30 +1,49 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, useTranslation } from "gatsby-plugin-react-i18next"
 
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Form from 'react-bootstrap/Form'
+import InputGroup from 'react-bootstrap/InputGroup'
+import FormControl from 'react-bootstrap/FormControl'
 import Container from 'react-bootstrap/Container'
+import Button from 'react-bootstrap/Button'
+import SubscribeIcon from "../svg/send.svg"
 import ListGroup from 'react-bootstrap/ListGroup'
 
 
 function Footer() {
-	const {t} = useTranslation("common");
+	const {t} = useTranslation("common")
+	const [submitted, setSubmit] = useState(false)
 
 	return (
 		<Container fluid as="footer" className="bg-lightgrey px-0">
-			<Container>
-				<Form style={{padding: "2rem"}}>
-					<Form.Group controlId="subscribeForm" className="d-flex flex-column align-items-center mb-0">
-						<Form.Label className="text-secondary text-uppercase text-center">{t('footer.subscribe')}</Form.Label>
-						<Form.Control type="email" placeholder={t('footer.placeholder')} style={{width: "20rem", boxShadow: "none"}} />
-					</Form.Group>
-				</Form>
-				<hr className="mt-0"/>
+			<Container className="d-flex flex-column pt-4">
+				<label htmlFor="subscribe" className="text-secondary text-uppercase text-center">
+					{t('footer.subscribe')}
+				</label>
+				{ submitted ? 
+					<p className="mx-auto">Thank you. We have added your email to the subscription list.</p> 
+					:
+					<InputGroup className="d-flex justify-content-center mb-4">
+						<FormControl
+							id="subscribe"
+							placeholder={t('footer.placeholder')}
+							aria-label="subscribe"
+							style={{maxWidth: "20rem", boxShadow: "none"}}
+						/>
+						<InputGroup.Append>
+							<Button variant="outline-secondary" className="subscribe-btn" onClick={() => setSubmit(true)}>
+								<SubscribeIcon />
+							</Button>
+						</InputGroup.Append>
+					</InputGroup>
+				}
+				<hr className="m-0" />
 			</Container>
-			<Container>
-				<Row xs={2} md={5}>
-					<Col>
+			<Container className="mt-4">
+				<Row xs={2} lg={5}>
+					<Col className="mb-2">
 						<ListGroup>
 							<strong>{t('menu.events')}</strong>
 							<ListGroup.Item>
@@ -35,7 +54,7 @@ function Footer() {
 							</ListGroup.Item>
 						</ListGroup>
 					</Col>
-					<Col>
+					<Col className="mb-2">
 						<ListGroup>
 							<strong>{t('menu.about')}</strong>
 							<ListGroup.Item>
@@ -44,22 +63,12 @@ function Footer() {
 							<ListGroup.Item>
 								<Link to="/" className="custom-link">Mission</Link>
 							</ListGroup.Item>
-							<ListGroup.Item>
+							{/* <ListGroup.Item>
 								<Link to="/" className="custom-link">Buffet</Link>
-							</ListGroup.Item>
+							</ListGroup.Item> */}
 						</ListGroup>
 					</Col>
-					<Col>
-						<ul className="list-group">
-							<strong>{t('menu.library')}</strong>
-						</ul>
-					</Col>
-					<Col>
-						<ul className="list-group">
-							<strong>{t('menu.hire')}</strong>
-						</ul>
-					</Col>
-					<Col>
+					<Col className="mb-2">
 						<ListGroup>
 							<strong>{t('menu.support-us')}</strong>
 							<ListGroup.Item>
@@ -70,16 +79,21 @@ function Footer() {
 							</ListGroup.Item>
 						</ListGroup>
 					</Col>
+					<Col>
+						<ListGroup>
+							<strong>{t('menu.library')}</strong>
+						</ListGroup>
+					</Col>
+					<Col>
+						<strong className="py-2">{t('footer.address-title')}</strong>
+						<a href="https://www.google.com/maps/place/Russian+House+Melbourne+Inc./@-37.8003492,144.9826023,15z/data=!4m5!3m4!1s0x0:0xe246adf64228441c!8m2!3d-37.8003492!4d144.9826023" className="custom-link text-decoration-none" target="_blank" rel="noopener noreferrer">
+							<p className="mb-0" style={{fontSize: "14px"}}>118 Greeves St,<br />
+								3065 Fitzroy</p>
+						</a>   
+					</Col>
 				</Row>
 				<hr />
 			</Container>
-			{/* <Col>
-				<strong className="py-2">{t('footer.address-title')}</strong>
-				<a href="https://www.google.com/maps/place/Russian+House+Melbourne+Inc./@-37.8003492,144.9826023,15z/data=!4m5!3m4!1s0x0:0xe246adf64228441c!8m2!3d-37.8003492!4d144.9826023" className="custom-link text-decoration-none" target="_blank" rel="noopener noreferrer">
-					<p className="mb-0" style={{fontSize: "14px"}}>118 Greeves St,<br />
-						3065 Fitzroy</p>
-				</a>   
-			</Col> */}
 			<Container className="d-flex flex-row justify-content-between text-muted">
 				<p className="small cursor-pointer">{t('footer.trademark')}</p>
 				<p className="small cursor-pointer">
