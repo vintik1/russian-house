69f764d00b6063dc0c091c25ce0584d995809620
Egor Sadanov <egorsadanov@yandex.ru>
2020-11-18 15:55:27 +1100

    add first mongodb function

--- lambda_functions/functions.js
+++
@@ -1,8 +0,0 @@
-// lambda functions 
-module.exports.handler = async (event, context) => {
-  const unixTime = Math.floor(Date.now() / 1000)
-  return {
-    statusCode: 200,
-    body: `The Unix time is ${unixTime}`,
-  }
-}
---
+++ lambda_functions/server.js
@@ -0,0 +1,47 @@
+// lambda functions 
+const MongoClient = require("mongodb").MongoClient
+
+const MONGODB_URI = process.env.MONGODB_URI
+const DB_NAME = 'rhlibrary'
+
+let cachedDb = null
+
+// Collection Name
+const collectionName = 'books'
+
+// const defaultPageSize = 10
+
+const connectToDatabase = async (uri) => {
+  if (cachedDb) return cachedDb
+
+  const client = await MongoClient.connect(uri, {
+    useUnifiedTopology: true,
+  })
+
+  cachedDb = client.db(DB_NAME)
+
+  return cachedDb
+}
+
+const queryDatabase = async (db) => {
+  const book = await db.collection(collectionName).find({}).toArray()
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(book),
+  }
+}
+
+module.exports.handler = async (event, context) => {
+  // const unixTime = Math.floor(Date.now() / 1000)
+  context.callbackWaitsForEmptyEventLoop = false
+  const db = await connectToDatabase(MONGODB_URI)
+  return queryDatabase(db)
+  // return {
+  //   statusCode: 200,
+  //   body: `The Unix time is ${unixTime}`,
+  // }
+}
